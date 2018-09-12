0b9c2d8f3b8c3e1638f08c6df9579309a3f38d16
Zaknefeinn <zaknefeinn@gmail.com>
2018-09-12 10:56:10 -0500

    push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 export default class Navbar extends Component {
